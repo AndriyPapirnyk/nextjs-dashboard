6f3d73f976ecdfbc8ec985582c33c5791f6fe693
AndriyPapirnyk <papirnykandriy@gmail.com>
2024-03-02 23:39:03 +0200

    adding hero images

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,7 @@
 import { Inter } from 'next/font/google';
+
+import { Montserrat } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
+
+export const montserrat = Montserrat({ subsets: ['latin'] });
